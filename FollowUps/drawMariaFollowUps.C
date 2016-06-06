f0148ea0a7329a18ed2434a3c5de1fbdce5c5199
Bobak <bth5032@gmail.com>
2016-06-06 12:04:50 +0200

    minor

--- FollowUps/drawMariaFollowUps.C
+++ FollowUps/drawMariaFollowUps.C
@@ -386,7 +386,7 @@ ERROR: Could not find plot info for "+plot_name+"\n\
   
 }
 
-void drawMariaFollUps(TString save_dir="~/public_html/ZMET2016/looper/maria/80DataVs76MC/", TString input_dir="~/maria/", bool pt=true, bool phi=true, bool sumET=true, bool MET=true, bool extra=true, bool do_extra=false)
+void drawMariaFollowUps(TString save_dir="~/public_html/ZMET2016/looper/maria/80DataVs76MC/", TString input_dir="~/maria/", bool pt=true, bool phi=true, bool sumET=true, bool MET=true, bool extra=true, bool do_extra=false)
 {
   
   vector<TString> plot_names;
